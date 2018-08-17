fca1acf899c86d11e2075e1b31b3234d241198ba
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2018-08-16 20:10:42 -0500

    Move subscribe/unsubscribe to componentDidMount/componentWillUnmount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,17 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = store.getState()
-    this.unsubscribe = store.subscribe(() => this.setState(store.getState()))
 
     this.handleAddRowClick = this.handleAddRowClick.bind(this)
     this.handleColorChange = this.handleColorChange.bind(this)
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState(store.getState()))
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
   }
 
   handleAddRowClick() {
@@ -22,16 +29,18 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (<div>
-      <h1>Pixelate</h1>
-      <div>
-        <button id='add-row' onClick={this.handleAddRowClick}>Add a row</button>
-        <ColorSelector colors={AVAILABLE_COLORS}
-                       selectedColor={this.state.selectedColor}
-                       onChange={this.handleColorChange}
-        />
+    return (
+      <div id="pixelate">
+        <h1>Pixelate</h1>
+        <div>
+          <button id='add-row' onClick={this.handleAddRowClick}>Add a row</button>
+          <ColorSelector colors={AVAILABLE_COLORS}
+                         selectedColor={this.state.selectedColor}
+                         onChange={this.handleColorChange}
+          />
+        </div>
+        <Table grid={this.state.grid} />
       </div>
-      <Table grid={this.state.grid} />
-    </div>)
+    )
   }
 }
